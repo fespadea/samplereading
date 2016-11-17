5b83b3f284ba5a301176f9f069e5b62d896486a7
fspadea <fs279@pvsd.org>
2016-11-17 14:29:22 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+import java.util.*;
+import java.io.*;
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(new File("MyData.txt"));
+
+        int maxIndex = -1;
+        String text [] = new String[1000];
+
+        while(sc.hasNext()){
+            maxIndex++;
+            text[maxIndex] = sc.next();
+        }
+        sc.close();
+        for(int j = 0; j <= maxIndex; j++){
+            System.out.println(text[j]);
+        }
+        FileWriter fw = new FileWriter("output", true);
+        PrintWriter pw = new PrintWriter(fw);
+
+        pw.println("Yo what up!");
+        pw.print(1);
+        Scanner kb = new Scanner(System.in);
+        System.out.println("Enter your password");
+        String password = kb.nextLine();
+        pw.println(password);
+        pw.close();
+        fw.close();
+    }
+}
